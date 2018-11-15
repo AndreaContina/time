6af38ee92ba44c168ffba54b70a5c2651c08ee89
AndreaContina <andrea.contina@ou.edu>
2018-11-14 23:49:35 -0600

    Update timeser_contina.R

--- timeser_contina.R
+++ timeser_contina.R
@@ -1,3 +1,5 @@
+# Class DA week 13
+# Time Series Lecture - Contina
 fit <- arima(log(interpTSDA1), c(0, 1, 1),seasonal = list(order = c(0, 1, 1), period = 12))
 pred <- predict(fit, n.ahead = 10*12)
 ts.plot(interpTSDA1,2.718^pred$pred, log = "y", lty = c(1,2), col=c("grey", "red"),
